3e8ab7484febd6bf50a7121ab8bc69b5a7eaa734
OscarHMG <oscar_mg7@hotmail.com>
2017-04-19 18:00:52 -0500

    init

---
+++ app/src/main/java/com/oscarhmg/desarrollo1/customthemes/ThemeApplication.java
@@ -0,0 +1,12 @@
+package com.oscarhmg.desarrollo1.customthemes;
+
+import android.app.Application;
+
+/**
+ * Created by desarrollo1 on 19/4/2017.
+ */
+
+public class ThemeApplication extends Application {
+    public static int currentPosition;
+
+}
